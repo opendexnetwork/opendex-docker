d33dcbb553c038444ecc3dc61002ae0a16a94b5e
FiatOG <EilahN9I@protonmail.com>
2021-02-18 20:24:56 +0800

    feat: interactive setup

--- opendex.sh
+++ opendex.sh
@@ -1,3 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "to be implemented"
+set -e
+
+OPENDEX_LAUNCHER_VERSION="v1.0.0-rc.3"
+
+# ensure the opendex-launcher binary is downloaded
+# select the network
+# run opendex-launcher setup --interactive
+
+if [ "$(uname)" = "Darwin" ]; then
+  OPENDEX_DOCKER_HOME="$HOME/Library/Application\ Support/OpendexDocker"
+else
+  OPENDEX_DOCKER_HOME="$HOME/.opendex-docker"
+fi
+
+if ! [ -e "$OPENDEX_DOCKER_HOME" ]; then
+  mkdir "$OPENDEX_DOCKER_HOME"
+fi
+
+ensure_launcher() {
+  :
+}
+
+ensure_network() {
+  :
+}
+
+ensure_launcher
+ensure_network
+$LAUNCHER setup --interactive
