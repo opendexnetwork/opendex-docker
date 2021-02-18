2af81859745a53252341cabea30bac2472736dbe
3nprob <3nprob@3nprob>
2021-02-18 14:10:59 +0900

    fix backup command

--- images/opendexd/opendexd-backup.sh
+++ images/opendexd/opendexd-backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./bin/opendexd-backup -b /root/backup
+XUD_BACKUP_DIR="${XUD_BACKUP_DIR:-/root/backup}"
+echo "[backup] Initiating backup to $XUD_BACKUP_DIR..."
+./bin/opendex-backup -b $XUD_BACKUP_DIR
