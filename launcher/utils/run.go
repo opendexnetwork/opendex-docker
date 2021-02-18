20da9b2dae0459082607721bc8ab8152676516b8
FiatOG <EilahN9I@protonmail.com>
2021-02-18 15:05:51 +0800

    fix: hardcoded container name
    This commit parses the container name from "docker-compose ps" command
    output to avoid unintentional prefixed contianer name (see #4) blocking
    the setup process.
    
    Closes #4.

--- launcher/utils/run.go
+++ launcher/utils/run.go
@@ -45,3 +45,39 @@ func Run(ctx context.Context, cmd *exec.Cmd) error {
 	}
 	return nil
 }
+
+func Output(ctx context.Context, cmd *exec.Cmd) (string, error) {
+	var buf bytes.Buffer
+
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+
+	if err := cmd.Start(); err != nil {
+		return "", fmt.Errorf("start: %w", err)
+	}
+	done := make(chan error)
+	output := ""
+	go func() { done <- cmd.Wait() }()
+	select {
+	case err := <-done:
+		// exited
+		output += strings.TrimSpace(buf.String())
+		if output != "" {
+			output = "\n" + output
+		}
+		if err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				logger.Errorf("[run] %s (exit %d)%s", cmd.String(), exitErr.ExitCode(), output)
+			}
+			return output, fmt.Errorf("[run] %s: %w", cmd.String(), err)
+		} else {
+			logger.Debugf("[run] %s%s", cmd.String(), output)
+		}
+	case <-ctx.Done():
+		// cancelled
+		if err := cmd.Process.Kill(); err != nil {
+			return "", fmt.Errorf("kill: %w", err)
+		}
+	}
+	return output, nil
+}
