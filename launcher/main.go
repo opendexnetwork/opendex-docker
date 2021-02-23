e2f3f241661a3b742662b1ed9e945aa8fc949765
FiatOG <EilahN9I@protonmail.com>
2021-02-23 16:32:01 +0800

    fix: client version 1.41 is too new

--- launcher/main.go
+++ launcher/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/opendexnetwork/opendex-docker/launcher/cmd"
+import (
+	"github.com/opendexnetwork/opendex-docker/launcher/cmd"
+	"os"
+)
+
 
 func main() {
+	if err := os.Setenv("DOCKER_API_VERSION", "1.40"); err != nil {
+		panic(err)
+	}
 	cmd.Execute()
 }
